c07ba5b31584812e171766154a936a1877f61cbe
ChristianStander <stander.christian@gmail.com>
2023-04-28 15:48:55 +0200

    Added comments

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 let groceryItems = ['mince', 'tomato', 'onion', 'pasta']
 const list = document.getElementById('itemList')
 
-
+// Function for updating the list with new values
 function updateList () {
     const input = document.getElementById('input')
     if (!input.value) {
@@ -27,6 +27,7 @@ function displayList (groceryItems) {
 }
 displayList(groceryItems)
 
+// Function to delete an item from the array and from the list
 function deleteItem(itemId) {
     const deleteElement = document.getElementById(itemId)
     const itemToRemove = deleteElement.textContent.slice(0, -2).trim()
@@ -35,16 +36,20 @@ function deleteItem(itemId) {
     displayList(groceryItems)
 }
 
+// Assigning itemList in order to create the toggle event
 const itemList = document.getElementById('itemList')
 
+// Event listener that can toggle whether an item is checked or not
 itemList.addEventListener('click', function (event) {
     if (event.target.tagName === "LI") {
         event.target.classList.toggle("checked")
     }
 })
 
+// Assigning a value that can be used to make use of ENTER
 const inputEnter = document.getElementById('input')
 
+// Event listener that notices an enter and calls the click() function
 inputEnter.addEventListener('keyup', function (event) {
     
     if (event.keyCode === 13) {
