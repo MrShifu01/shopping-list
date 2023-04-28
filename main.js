d770a5435b7de80c4d6a25d40ebbc1b509c7cffa
ChristianStander <stander.christian@gmail.com>
2023-04-28 09:40:52 +0200

    functionality is great, now gonna style it

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Creating an array and adding 4 grocery items
-const groceryItems = ['mince', 'tomato', 'onion', 'pasta']
+let groceryItems = ['mince', 'tomato', 'onion', 'pasta']
 const list = document.getElementById('itemList')
 
 
@@ -20,16 +20,30 @@ function displayList (groceryItems) {
     list.textContent = ''
     for (let i = 0; i < groceryItems.length; i++) {
         const listItem= document.createElement('li')
-        listItem.innerHTML = `${groceryItems[i]} <span onClick="deleteItem()" class="close">\u00D7</span> `
+        listItem.innerHTML = `${groceryItems[i]} <span onClick="deleteItem('item${i}')" class="close">\u00D7</span>`
+        listItem.id = `item${i}`
         list.appendChild(listItem)
     }
 }
 displayList(groceryItems)
 
-function deleteItem () {
-    console.log(groceryItems)
-    console.log("hello")
+function deleteItem(itemId) {
+    const deleteElement = document.getElementById(itemId)
+    const itemToRemove = deleteElement.textContent.slice(0, -2).trim()
+    groceryItems = groceryItems.filter(item => item !== itemToRemove)
+    deleteElement.style.display = "none"
+    displayList(groceryItems)
 }
+
+const itemList = document.getElementById('itemList')
+
+itemList.addEventListener('click', function (event) {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("checked")
+    }
+})
+
+
 
 // // Function to change the styling of items that have been bought already
 // function boughtItems () {
